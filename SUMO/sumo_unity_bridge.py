d799ccb207f1a8af7df0f8e1898dd5b62b03ad72
AbdulMuizz789 <muizzabdul789@gmail.com>
2026-02-27 19:47:35 +0530

    fix: send parked vehicle data to Unity in SUMO bridge step method

--- SUMO/sumo_unity_bridge.py
+++ SUMO/sumo_unity_bridge.py
@@ -17,6 +17,7 @@ class SUMOUnityBridge:
         
     def step(self):
         traci.simulationStep()
+
         departed = traci.simulation.getDepartedIDList()
         
         for veh_id in departed:
@@ -24,5 +25,23 @@ class SUMOUnityBridge:
                 'id': veh_id,
                 'type': traci.vehicle.getTypeID(veh_id),
                 'position': traci.vehicle.getPosition(veh_id)
+            }
+            self.send_to_unity(vehicle_data)
+
+        # Check for vehicles that have just parked
+        parked_vehicles = traci.simulation.getParkingStartingVehiclesIDList()
+        
+        for veh_id in parked_vehicles:
+            # Get the parking area ID the vehicle stopped at
+            stop_params = traci.vehicle.getStops(veh_id)
+            if stop_params:
+                parking_area_id = stop_params[0].stoppingPlaceID
+
+            # Send this data to Unity
+            vehicle_data = {
+                'id': veh_id,
+                'action': 'park',
+                'parking_area_id': parking_area_id,
+                # You might need logic to determine which specific index within the area
             }
             self.send_to_unity(vehicle_data)
