c47a63948dc92b0555d81f87c399f39cfed84723
AbdulMuizz789 <muizzabdul789@gmail.com>
2026-02-28 01:43:24 +0530

    Unity update

--- SimIPS/Assets/Scripts/Utilities/SUMOConnector.cs
+++ SimIPS/Assets/Scripts/Utilities/SUMOConnector.cs
@@ -1,16 +1,44 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class SUMOConnector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int unityPort = 9000;
+    private TcpListener _listener;
+    private TcpClient _client;
+    private NetworkStream _stream;
+
     void Start()
     {
-        
+        // 1) start listening on port 9000
+        _listener = new TcpListener(IPAddress.Any, unityPort);
+        _listener.Start();
+        Debug.Log($"[SUMOConnector] Listening on port {unityPort} …");
+
+        // 2) accept the incoming connection asynchronously
+        _listener.BeginAcceptTcpClient(OnSUMOConnected, null);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSUMOConnected(IAsyncResult ar)
     {
-        
+        _client = _listener.EndAcceptTcpClient(ar);
+        _stream = _client.GetStream();
+        Debug.Log("[SUMOConnector] Python bridge connected!");
+        // from here on you can read/write to _stream
+    }
+
+    public void Step()
+    {
+        // called once per frame (or in your SimulationController)
+        // read messages from _stream or write data back
+    }
+
+    void OnApplicationQuit()
+    {
+        _stream?.Close();
+        _client?.Close();
+        _listener?.Stop();
     }
 }
